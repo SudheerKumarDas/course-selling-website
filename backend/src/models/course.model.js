278b61e32ac603bdea0af8e15d2ee0d18f0adf2f
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-13 11:29:10 +0545

    added the course schema

---
+++ backend/src/models/course.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:String,
+        required:true
+    },
+    imageLink:{
+        type:String,
+    },
+    published:{
+        type:Boolean,
+        required:true
+    }
+})
+
+const Course = mongoose.model("Course",courseSchema);
+
+export default Course;
