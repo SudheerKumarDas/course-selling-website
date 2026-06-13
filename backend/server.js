c5f98d2eaebd1c6a1838ad9518caa4abfc2794c9
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-13 09:14:46 +0545

    database connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv"
+
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("Server is listening to the port:3000");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is listening to the PORT : ",PORT);
+  });
+});
