69e757f834026e00ae4d9a0d2aed2357fba34986
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-22 17:30:26 +0545

    user creating from frontend

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,30 +1,64 @@
+import { useState } from "react"
 
 function SignUp() {
+  const [username,setUsername]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try{
+      const response = await fetch(
+        "http://localhost:3000/user/signup",
+        {
+          method:"POST",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify({
+            username,
+            email,
+            password
+          })
+        }
+      )
+      const data = await response.json();
+      console.log(data);
+      if(response.ok){
+        alert("sign up successful")
+      }else{
+        alert(data.message)
+      }
+    }catch(error){
+      console.error(error);
+    }
+  }
+  
   return (
     <div>
         <h1>Course Selling App</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <label>Username : </label>
-            <input type="text" placeholder="Enter username" className="border-2"/>
+            <input type="text" placeholder="Enter username" className="border-2 p-2" value={username} onChange={(e)=>setUsername(e.target.value)}/>
 
             <br />
 
             <label>Email : </label>
-            <input type="text" placeholder="Enter email" className="border-2"/>
+            <input type="text" placeholder="Enter email" className="border-2 p-2" value={email} onChange={(e)=>setEmail(e.target.value)}/>
 
             <br />
 
             <label>Password : </label>
-            <input type="password" placeholder="Enter password" className="border-2"/>
+            <input type="password" placeholder="Enter password" className="border-2 p-2" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
             <br />
 
-            <button className="cursor-pointer border-2"> User Sign Up</button>
+            <button className="cursor-pointer border-2 p-2" type="submit"> User Sign Up</button>
 
             <br />
 
-            <button className="cursor-pointer border-2"> Admin Sign Up</button>
+            <button className="cursor-pointer border-2 p-2"> Admin Sign Up</button>
 
         </form>
     </div>
