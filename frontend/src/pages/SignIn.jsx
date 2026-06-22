022a2a9b53e40014c2bbe9ac23f483c5408c0968
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-22 17:49:52 +0545

    made sign in page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,25 +1,25 @@
+import { useState } from "react"
 
 function SignIn() {
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+
   return (
     <div>
         <form>
 
             <label>Email : </label>
-            <input type="text" placeholder="Enter your email" className="border-2"/>
+            <input type="text" placeholder="Enter your email" className="border-2" value={email} onChange={(e)=>setEmail(e.target.value)}/>
 
             <br />
 
             <label>Password : </label>
-            <input type="password" placeholder="Enter your password" className="border-2"/>
+            <input type="password" placeholder="Enter your password" className="border-2" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
             <br />
 
             <button className="cursor-pointer border-2">User sign in</button>
-
-            <br />
-
-            <button className="cursor-pointer border-2">Admin sign in</button>
-
+            
         </form>
     </div>
   )
