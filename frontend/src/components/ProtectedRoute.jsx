2838fadc8e14d7ebf0c924e22f238192fbb26bed
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-23 20:37:20 +0545

    make a protected route component

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({children}) {
+    const token = localStorage.getItem("token");
+    if(!token){
+        return <Navigate to="/signin" />
+    }
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default ProtectedRoute
