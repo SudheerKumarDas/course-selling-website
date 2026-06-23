cbb22dde6633ae1a8d08373432756a8edcf81287
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-23 10:38:50 +0545

    Dashboard gives all the available courses

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react"
+
 function Dashboard() {
+    const [courses,setCourses]=useState([]);
+    useEffect(()=>{
+        const getCourses = async() => {
+            try {
+                const token = localStorage.getItem("token");
+                const response = await fetch(
+                "http://localhost:3000/user/courses",
+                {
+                    method:"GET",
+                    headers:{
+                        "Authorization":`Bearer ${token}`
+                    }
+                }
+            )
+            const data = await response.json();
+            console.log(data);
+            if(response.ok){
+                setCourses(data.courses)
+            }else{
+                console.log(data.message);
+            }
+                
+            } catch (error) {
+                console.error(error);
+            }            
+        }
+        getCourses();
+    },[]);
   return (
-    <div>Dashboard</div>
+    <div>
+        <h1>Dashboard</h1>
+        {courses.map((course)=>(
+            <div key={course._id}>
+                <h2 className="text-3xl font-bold text-blue-600">{course.title}</h2>
+                <p>{course.description}</p>
+                <p>{course.price}</p>
+                <a>{course.imageLink}</a>
+            </div>
+        ))}
+    </div>
   )
 }
 
