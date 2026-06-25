45af58edaef73064c722656e2deaf1ce422c8d40
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-25 07:38:20 +0545

    skeleton for create course

--- frontend/src/pages/AdminCreateCourse.jsx
+++ frontend/src/pages/AdminCreateCourse.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react"
 
 function AdminCreateCourse() {
+    const [title,setTitle]=useState("");
+    const [description,setDescription]=useState("");
+    const [price,setPrice]=useState(0);
+    const [imageLink,setImageLink]=useState("");
+    const [published,setPublished]=useState(false);
   return (
-    <div>AdminCreateCourse</div>
+    <div>
+
+        <form>
+
+            <label htmlFor="title">Title : </label>
+            <input type="text" name="title" value={title} placeholder="Enter course title" onChange={(e)=>setTitle(e.target.value)}/>
+
+            <br />
+
+            <label htmlFor="description">Description : </label>
+            <input type="text" name="description" value={description} placeholder="Enter course description" onChange={(e)=>setDescription(e.target.value)}/>
+
+            <br />
+            
+            <label htmlFor="price">Price : </label>
+            <input type="text" name="price" value={price} placeholder="Enter course price" onChange={(e)=>setPrice(e.target.value)}/>
+
+            <br />
+            
+            <label htmlFor="image-link">ImageLink : </label>
+            <input type="text" name="image-link" value={imageLink} placeholder="Enter course image link" onChange={(e)=>setImageLink(e.target.value)}/>
+
+            <br />
+            
+            <label htmlFor="published">Published : </label>
+            <input type="text" name="published" value={published} placeholder="Enter course published status" onChange={(e)=>setPublished(e.target.value)}/>
+
+            <br />
+            
+        </form>
+    </div>
   )
 }
 
