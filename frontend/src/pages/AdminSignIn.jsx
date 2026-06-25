e74ddf8a2a4ca60e96e254e14c89b051c22635d4
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-25 06:42:33 +0545

    Admin sign in functionality implemented

--- frontend/src/pages/AdminSignIn.jsx
+++ frontend/src/pages/AdminSignIn.jsx
@@ -4,6 +4,32 @@ function AdminSignIn() {
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                "http://localhost:3000/admins/signin",
+                {
+                    method:"POST",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify({
+                        email,
+                        password
+                    })
+                }
+            )
+            const data = await response.json();
+            if(response.ok){
+                alert("Admin logged in successfully")
+            }else{
+                alert(data.message);
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
   return (
     <div>
         <form onSubmit={handleSubmit}>
